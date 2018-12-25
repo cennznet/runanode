21ff73a0a048932951191919cd31ad28b89d0c19
Ken Huang <ken.huang@centrality.ai>
2018-12-26 11:02:09 +1300

    able to open debug mode on prod, fix icon and menu

--- app/main/menu.js
+++ app/main/menu.js
@@ -45,17 +45,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate() {
     const subMenuAbout = {
-      label: 'Electron',
+      label: 'Odin',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Odin',
           selector: 'orderFrontStandardAboutPanel:'
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Odin',
           accelerator: 'Command+H',
           selector: 'hide:'
         },
