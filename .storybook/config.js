2dd8f63614c3ecacd20a18cae1f1882c52f57ae3
Ken Huang <ken.huang@centrality.ai>
2018-12-30 21:50:01 +1300

    add storybook addons and examples

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+
+addDecorator(withNotes);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
