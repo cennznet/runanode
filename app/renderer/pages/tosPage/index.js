a876683fc0147f62bc6e709abf9ff8188261d114
Ben Gao <xingbingao@gmail.com>
2019-01-03 11:25:58 +1300

    NODE-29 Add terms and conditions page

--- app/renderer/pages/tosPage/index.js
+++ app/renderer/pages/tosPage/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colors } from 'renderer/theme';
 import { Layout, LayoutWrapper, MainContent, SimpleSidebar } from 'components/layout';
+import TosContent from 'components/TosContent';
+import Button from 'components/Button';
 
-const WalletCreatePage = () => (
-  <Layout sidebar={<SimpleSidebar />}>
-    <LayoutWrapper>
-      <MainContent>wallet create</MainContent>
-    </LayoutWrapper>
-  </Layout>
-);
+const ScrollWrapper = styled.div`
+  width: 100%;
+  height: 80vh;
+`;
 
-export default WalletCreatePage;
+const ScrollContent = styled.div`
+  margin-top: 0.5rem;
+  max-height: calc(80vh - 40px);
+  overflow-y: auto;
+  padding: 0.5rem 0;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    background-color: ${colors.N900};
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${colors.N700};
+    border-radius: 3px;
+  }
+`;
+
+const Note = styled.div`
+  font-size: 1rem;
+  margin-bottom: 1.5rem;
+`;
+
+export default class TosPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { locked: true };
+    this.observe = null;
+  }
+
+  scrollObserver = ref => {
+    if (ref) {
+      new IntersectionObserver(
+        (entries, observer) => {
+          const locked = !entries.some(({ intersectionRatio }) => intersectionRatio > 0.9);
+          if (!locked) {
+            this.setState({ locked });
+            observer.disconnect();
+          }
+        },
+        { threshold: 1.0 }
+      ).observe(ref);
+    }
+  };
+
+  render() {
+    return (
+      <Layout sidebar={<SimpleSidebar />}>
+        <LayoutWrapper>
+          <MainContent>
+            <ScrollWrapper>
+              <ScrollContent>
+                <TosContent />
+                <div ref={this.scrollObserver} />
+              </ScrollContent>
+            </ScrollWrapper>
+            <Note>By clicking I Agree, you agree to our terms and conditions</Note>
+            <Button disabled={this.state.locked}>I Agree</Button>
+          </MainContent>
+        </LayoutWrapper>
+      </Layout>
+    );
+  }
+}
