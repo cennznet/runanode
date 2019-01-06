5845d40318e5866e4a13f9f49c411bbd11d19c1f
Cherry Liang <cherry@centrality.ai>
2019-01-07 12:00:35 +1300

    add props to component

--- app/renderer/components/Select/index.js
+++ app/renderer/components/Select/index.js
@@ -10,8 +10,12 @@ const StyledSelect = styled(ReactSelect)`
 
   .react-select__control {
     box-shadow: none;
-    color: ${p => p.border || p.theme.colors.N500};
-    border-color: ${p => p.border || p.theme.colors.N100};
+    color: ${p => p.color || p.theme.colors.N500};
+    border-color: ${p => p.borderColor || p.theme.colors.N100};
+
+    &:hover {
+      border-color: ${p => p.borderHoverColor || p.theme.colors.N100};
+    }
   }
 
   .react-select__control--is-disabled {
@@ -19,7 +23,7 @@ const StyledSelect = styled(ReactSelect)`
   }
 
   .react-select__menu {
-    background-color: ${p => p.theme.colors.N0};
+    background-color: ${p => p.backgroundColor || p.theme.colors.N0};
   }
 
   .react-select__option--is-focused {
