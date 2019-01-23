66c2bf50b58440bb8d14cd65c4839d63bef06b3b
Cherry Liang <cherry@centrality.ai>
2019-01-24 12:36:04 +1300

    add Modal component to ui-book

--- app/renderer/components/Modal/index.js
+++ app/renderer/components/Modal/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 import Button from 'components/Button';
+import styled from 'styled-components';
+import { colors } from 'renderer/theme';
+import ModalFooter from './lib/modalFooter';
 
-const Modal = ({ isOpen, onOpenModal, onCloseModal, styles, children, ...restProps }) => (
-  <Modal
-    isOpen={isOpen}
-    onAfterOpen={onOpenModal}
-    onRequestClose={onCloseModal}
-    style={styles}
-    contentLabel="Example Modal"
-  >
-    <Button onClick={onCloseModal}>close</Button>
-  </Modal>
-);
+const ModalContent = styled.div`
+  padding: 1rem;
+`;
 
-export default Modal;
+const Modal = ({
+  isOpen,
+  onOpenModal,
+  onCloseModal,
+  styles,
+  children,
+  className,
+  footer,
+  ...restFooterProps
+}) => {
+  const contentClassName = `${className}__content`;
+  const overlayClassName = `${className}__overlay`;
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onAfterOpen={onOpenModal}
+      onRequestClose={onCloseModal}
+      className={contentClassName}
+      overlayClassName={overlayClassName}
+      ariaHideApp={false}
+    >
+      <ModalContent>{children}</ModalContent>
+      {footer && <ModalFooter {...restFooterProps} />}
+    </ReactModal>
+  );
+};
+
+const StyledModal = styled(Modal)`
+  &__overlay {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    background-color: ${p => p.overlayBgColor || 'rgba(255, 255, 255, 0.55)'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__content {
+    position: absolute;
+    border-radius: 3px;
+    min-width: ${p => p.minWith || '50vw'};
+    min-height: ${p => p.minWith || '25vh'};
+    background-color: ${p => p.backgroundColor || '#040c40'};
+    box-shadow: ${p => p.boxShadow || `0 2px 4px 0 ${colors.N900}`};
+    color: ${p => p.color || 'white'};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    :focus {
+      outline: 0;
+    }
+  }
+`;
+
+export default StyledModal;
