9d113a219194d8f3b8db7f152be14878624d4549
Ben Gao <xingbingao@gmail.com>
2019-01-25 14:02:15 +1300

    Generate wallets

--- app/renderer/pages/wallet/create/seedPhrasePage/index.js
+++ app/renderer/pages/wallet/create/seedPhrasePage/index.js
@@ -3,26 +3,30 @@ import { Button, PageHeading, PageFooter } from 'components';
 import StartOverLink from 'renderer/pages/wallet/StartOverLink';
 import { STEPS } from '../constants';
 import SeedPhraseList from './SeedPhraseList';
+import StoreSeedPhraseModal from './StoreWarningModal';
 
-const SeedPhrasePage = ({ mnemonicString, moveToStep }) => (
-  <React.Fragment>
-    <PageHeading subHeading="It is essential that you write this down and store it somewhere securely, like a safe or safety deposit box. If you lose access to your wallet, this seed phrase is the only way to recover your funds. Your seed phrase will only display once.">
-      Store your seed phrase offline
-    </PageHeading>
-    <div className="content">
-      <SeedPhraseList {...{ mnemonicString }} />
-    </div>
-    <PageFooter>
-      <StartOverLink />
-      <Button
-        onClick={() => {
-          moveToStep(STEPS.SEED_PHRASE_QUIZ);
-        }}
-      >
-        Next
-      </Button>
-    </PageFooter>
-  </React.Fragment>
-);
+const SeedPhrasePage = props => {
+  const {
+    mnemonicString,
+    onCreateWallet,
+    isStoreWarningModalOpen,
+    setStoreWarningModalOpen,
+  } = props;
+  return (
+    <React.Fragment>
+      <PageHeading subHeading="It is essential that you write this down and store it somewhere securely, like a safe or safety deposit box. If you lose access to your wallet, this seed phrase is the only way to recover your funds. Your seed phrase will only display once.">
+        Store your seed phrase offline
+      </PageHeading>
+      <div className="content">
+        <SeedPhraseList {...{ mnemonicString }} />
+      </div>
+      <PageFooter>
+        <StartOverLink />
+        <Button onClick={() => setStoreWarningModalOpen(true)}>Next</Button>
+      </PageFooter>
+      <StoreSeedPhraseModal {...props} />
+    </React.Fragment>
+  );
+};
 
 export default SeedPhrasePage;
