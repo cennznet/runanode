936c214cf7a1ad730cf44e77aed6c0e28fa960d1
Cherry Liang <cherry@centrality.ai>
2019-01-31 10:28:44 +1300

    rename storeWalletEpic

--- app/renderer/epics/walletCreate.epics.js
+++ app/renderer/epics/walletCreate.epics.js
@@ -55,4 +55,4 @@ const pageRedirectionAfterWalletCreatedEpic = action$ =>
     })
   );
 
-export default [createWalletEpic, walletCreateCompletedEpic, pageRedirectionAfterWalletCreatedEpic];
+export default [createWalletEpic, storeWalletEpic, pageRedirectionAfterWalletCreatedEpic];
