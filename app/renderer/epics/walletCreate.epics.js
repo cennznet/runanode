59bfc10f2045e6bede90a71b6bb81c0235a603ef
Cherry Liang <cherry@centrality.ai>
2019-01-31 09:52:23 +1300

    getWalletAddress api

--- app/renderer/epics/walletCreate.epics.js
+++ app/renderer/epics/walletCreate.epics.js
@@ -1,10 +1,34 @@
-import { EMPTY, from, of } from 'rxjs';
-import { concat, mergeMap } from 'rxjs/operators';
+import { EMPTY, from, of, combineLatest } from 'rxjs';
+import { mergeMap, catchError, concat } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { Wallet } from 'cennznet-wallet';
 import ROUTES from 'renderer/constants/routes';
+import chainEpics from 'renderer/epics/chainEpics';
 import types from '../types';
 import { getStorage, storageKeys } from '../api/utils/storage';
+
+// const createWalletEpic = action$ =>
+//   action$.pipe(
+//     ofType(types.walletCreate.requested),
+//     mergeMap(async ({ payload }) => {
+//       const { name, mnemonic, passphrase } = payload;
+//       const wallet = await window.odin.api.cennz.createWallet({
+//         name,
+//         mnemonic,
+//         passphrase: passphrase || '',
+//       });
+//       return {
+//         type: types.walletCreate.completed,
+//         payload: { wallet },
+//       };
+//     })
+//   );
+
+// const getAddressAfterWalletCreation = chainEpics(
+//   types.walletCreate.completed,
+//   types.getWalletAddress.requested,
+//   payload => payload && payload.wallet._accountKeyringMap
+// );
 
 const createWalletEpic = action$ =>
   action$.pipe(
@@ -20,6 +44,10 @@ const createWalletEpic = action$ =>
         mnemonic,
         passphrase: passphrase || '',
       });
+      const accountKeyringMap = wallet && wallet.wallet._accountKeyringMap;
+      const walletAddress = await window.odin.api.cennz.getWalletAddress({ accountKeyringMap });
+      wallet.wallet.walletAddress = walletAddress;
+
       wallets.push(wallet);
 
       return {
@@ -29,22 +57,15 @@ const createWalletEpic = action$ =>
     })
   );
 
-const walletCreateCompletedEpic = action$ =>
+const storeWalletEpic = action$ =>
   action$.pipe(
     ofType(types.walletCreate.completed),
-    mergeMap(({ payload }) => {
+    mergeMap(({ payload: { wallets } }) => {
       return of({
         type: types.setStorage.requested,
-        payload: { key: storageKeys.WALLETS, value: payload.wallets },
-      }).pipe(
-        concat(
-          of({
-            type: types.navigation.triggered,
-            payload: ROUTES.WALLET.ROOT,
-          })
-        )
-      );
+        payload: { key: storageKeys.WALLETS, value: wallets },
+      }).pipe(concat(of({ type: types.navigation.triggered, payload: ROUTES.WALLET.ROOT })));
     })
   );
 
-export default [createWalletEpic, walletCreateCompletedEpic];
+export default [createWalletEpic, storeWalletEpic];
