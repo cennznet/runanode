248294835c004054e6edd982b9a1e2bc19e76fd6
Cherry Liang <cherry@centrality.ai>
2019-02-01 11:48:17 +1300

    switch network

--- app/common/types/cennznet-node.types.js
+++ app/common/types/cennznet-node.types.js
@@ -6,13 +6,17 @@ export type TlsConfig = {
   key: Uint8Array,
 };
 
-export type NetworkNames = (
-  'mainnet' | 'staging' | 'testnet' | 'development' | 'cennznet-dev' | 'cennznet-uat' | 'local-testnet' | string
-);
+export type NetworkNames =
+  | 'mainnet'
+  | 'staging'
+  | 'testnet'
+  | 'development'
+  | 'cennznet-dev'
+  | 'cennznet-uat'
+  | 'local-testnet'
+  | string;
 
-export type PlatformNames = (
-  'win32' | 'linux' | 'darwin' | string
-);
+export type PlatformNames = 'win32' | 'linux' | 'darwin' | string;
 
 export const NetworkNameOptions = {
   mainnet: 'mainnet',
@@ -21,25 +25,32 @@ export const NetworkNameOptions = {
   development: 'development',
   CENNZNET_DEV: 'cennznet-dev',
   CENNZNET_UAT: 'cennznet-uat',
-  LOCAL_TESTNET: 'local-testnet'
+  LOCAL_TESTNET: 'local-testnet',
 };
 
-export type CennzNetNodeState = (
-  'stopped' | 'starting' | 'running' | 'stopping' | 'updating' |
-  'updated' | 'crashed' | 'errored' | 'exiting' | 'unrecoverable'
-);
+export type CennzNetNodeState =
+  | 'stopped'
+  | 'starting'
+  | 'running'
+  | 'stopping'
+  | 'updating'
+  | 'updated'
+  | 'crashed'
+  | 'errored'
+  | 'exiting'
+  | 'unrecoverable';
 
 export const CennzNetNodeStates: {
   STARTING: CennzNetNodeState,
-  RUNNING: CennzNetNodeState;
-  EXITING: CennzNetNodeState;
-  STOPPING: CennzNetNodeState;
-  STOPPED: CennzNetNodeState;
-  UPDATING: CennzNetNodeState;
-  UPDATED: CennzNetNodeState;
-  CRASHED: CennzNetNodeState;
-  ERRORED: CennzNetNodeState;
-  UNRECOVERABLE: CennzNetNodeState;
+  RUNNING: CennzNetNodeState,
+  EXITING: CennzNetNodeState,
+  STOPPING: CennzNetNodeState,
+  STOPPED: CennzNetNodeState,
+  UPDATING: CennzNetNodeState,
+  UPDATED: CennzNetNodeState,
+  CRASHED: CennzNetNodeState,
+  ERRORED: CennzNetNodeState,
+  UNRECOVERABLE: CennzNetNodeState,
 } = {
   STARTING: 'starting',
   RUNNING: 'running',
@@ -53,24 +64,21 @@ export const CennzNetNodeStates: {
   UNRECOVERABLE: 'unrecoverable',
 };
 
-export type CennzNetPidOptions = (
-  'mainnet-PREVIOUS-CENNZNET-PID' |
-  'staging-PREVIOUS-CENNZNET-PID' |
-  'testnet-PREVIOUS-CENNZNET-PID' |
-  'development-PREVIOUS-CENNZNET-PID' |
-  string
-);
+export type CennzNetPidOptions =
+  | 'mainnet-PREVIOUS-CENNZNET-PID'
+  | 'staging-PREVIOUS-CENNZNET-PID'
+  | 'testnet-PREVIOUS-CENNZNET-PID'
+  | 'development-PREVIOUS-CENNZNET-PID'
+  | string;
 
 export type CennzNetNodeStorageKeys = {
-  PREVIOUS_CENNZNET_PID: CennzNetPidOptions
+  PREVIOUS_CENNZNET_PID: CennzNetPidOptions,
 };
 
-export type CennzNetNodeProcessNames = (
-  'cennznet-node' | 'cennznet-node.exe'
-);
+export type CennzNetNodeProcessNames = 'cennznet-node' | 'cennznet-node.exe';
 
 export type ProcessNames = {
-  CENNZNET_PROCESS_NAME: CennzNetNodeProcessNames
+  CENNZNET_PROCESS_NAME: CennzNetNodeProcessNames,
 };
 
 export const CennzNetProcessNameOptions: {
@@ -80,25 +88,24 @@ export const CennzNetProcessNameOptions: {
 } = {
   win32: 'cennznet-node.exe',
   linux: 'cennznet-node',
-  darwin: 'cennznet-node'
+  darwin: 'cennznet-node',
 };
 
 /**
  * Expected fault injection types that can be used to tell
  * cennznet-node to behave faulty (useful for testing)
  */
-export type FaultInjection = (
-  'FInjIgnoreShutdown' |
-  'FInjIgnoreAPI' |
-  'FInjApplyUpdateNoExit' |
-  'FInjApplyUpdateWrongExitCode'
-);
+export type FaultInjection =
+  | 'FInjIgnoreShutdown'
+  | 'FInjIgnoreAPI'
+  | 'FInjApplyUpdateNoExit'
+  | 'FInjApplyUpdateWrongExitCode';
 
 export const FaultInjections: {
   IgnoreShutdown: FaultInjection,
   IgnoreApi: FaultInjection,
   ApplyUpdateNoExit: FaultInjection,
-  ApplyUpdateWrongExitCode: FaultInjection
+  ApplyUpdateWrongExitCode: FaultInjection,
 } = {
   IgnoreShutdown: 'FInjIgnoreShutdown',
   IgnoreApi: 'FInjIgnoreAPI',
@@ -120,5 +127,5 @@ export type CennzNetStatus = {
 
 export type CennzNetRestartOptions = {
   name: string,
-  chain: string
-}
+  chain: string,
+};
