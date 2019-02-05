847fd89532f7131a3a33af76911ad9cebc7d318f
Ben Gao <xingbingao@gmail.com>
2019-02-05 13:08:58 +1300

    Add tabs component

--- app/renderer/pages/wallet/details/AccountDetails.js
+++ app/renderer/pages/wallet/details/AccountDetails.js
@@ -3,10 +3,9 @@ import R from 'ramda';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MainContent, MainLayout } from 'components/layout';
-import { PageHeading, PageFooter } from 'components';
-import withContainer from './container';
+import { PageHeading, PageFooter, Tabs, TabPane } from 'components';
 
-const WalletAccountPage = ({ wallet }) => {
+const AccountDetails = ({ wallet }) => {
   const publicWalletAddress =
     wallet.wallet.walltAddress || Object.keys(wallet.wallet._accountKeyringMap);
 
@@ -15,8 +14,16 @@ const WalletAccountPage = ({ wallet }) => {
       <PageHeading subHeading={`Public Address: ${publicWalletAddress[0]}`}>
         {wallet.name}
       </PageHeading>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Portfolio" key="1">
+          <div>tab 1</div>
+        </TabPane>
+        <TabPane tab="Receive" key="2">
+          <div>tab 2</div>
+        </TabPane>
+      </Tabs>
     </React.Fragment>
   );
 };
 
-export default withContainer(WalletAccountPage);
+export default AccountDetails;
--- styleguideHelpers/setup.js
+++ styleguideHelpers/setup.js
@@ -2,6 +2,10 @@ import DemoCanvas from './DemoCanvas';
 import DemoSquare from './DemoSquare';
 import DemoFlex from './DemoFlex';
 
+import TabPane from '../app/renderer/components/Tabs/TabPane';
+
 global.DemoCanvas = DemoCanvas;
 global.DemoSquare = DemoSquare;
 global.DemoFlex = DemoFlex;
+
+global.TabPane = TabPane;
