61030b229d9736ba4ba7425735e244b8f7bcbcc5
Ben Gao <xingbingao@gmail.com>
2019-02-05 13:02:28 +1300

    Refactor account page

---
+++ app/renderer/pages/wallet/details/accountPage/container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+);
