8df17ac803dd5a3a266e83942b5700105c3f89bf
Ken Huang <ken.huang@centrality.ai>
2019-02-08 17:11:27 +1300

    Able to display balance in Account detail page

--- app/renderer/api/wallets/CennznetWalletAsset.js
+++ app/renderer/api/wallets/CennznetWalletAsset.js
@@ -1,21 +1,34 @@
 // @flow
 import BN from 'bn.js';
+import jdenticon from "jdenticon";
 
 export default class CennznetWalletAsset {
   assetId: BN;
   address: string;
+  iconBase64: string;
   name: string;
   freeBalance: BN;
   reservedBalance: BN;
+  totalBalance: BN;
 
   constructor(data: {
     assetId: BN,
     address: string,
     name: string,
-    freeBalance: ?BN,
-    reservedBalance: ?BN,
+    freeBalance: BN,
+    reservedBalance: BN,
+    totalBalance: BN,
   }) {
+    this.iconBase64 = jdenticon.toPng(data.assetId.toString(10),200).toString('base64');
     Object.assign(this, data);
   }
+
+  // get totalBalance(): BN {
+  //   return this.freeBalance.add(this.reservedBalance?this.reservedBalance: new BN(0, 10));
+  // }
+  //
+  // get freeBalanceDisplay(): BN {
+  //   return this.freeBalance;
+  // }
 
 }
