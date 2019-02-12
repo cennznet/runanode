9a979c3b5a06a62b4969fd9b67cf71b33b90f1fc
Ken Huang <ken.huang@centrality.ai>
2019-02-12 16:00:46 +1300

    fix review issue

--- app/renderer/epics/localStorage.epics.js
+++ app/renderer/epics/localStorage.epics.js
@@ -66,32 +66,4 @@ const resetLocalStorageEpic = action$ =>
     })
   );
 
-const syncWalletDataEpic = action$ =>
-  action$.pipe(
-    ofType(types.syncWalletData.requested),
-    mergeMap(async ({ payload }) => {
-      Logger.debug(`syncWalletDataEpic`);
-      let wallets = await getStorage(storageKeys.WALLETS);
-      if (wallets === null) {
-        wallets = [];
-      }
-
-      const myWallet = wallets.find(x => x.id === payload.id);
-      if(myWallet) {
-        Logger.debug(`myWallet: ${myWallet}`);
-        const myWalletIndex = wallets.findIndex(x => x.id === payload.id);
-        Logger.debug(`myWalletIndex: ${myWalletIndex}`);
-        const syncedWallet = await window.odin.api.cennz.syncWalletData(myWallet);
-        Logger.debug(`wallets[myWalletIndex]: ${myWalletIndex}, ${syncedWallet}`);
-        wallets[myWalletIndex] = syncedWallet;
-      }
-      return (
-        {
-          type: types.setStorage.requested,
-          payload: { key: storageKeys.WALLETS, value: wallets },
-        }
-      );
-    })
-  );
-
-export default [getStorageEpic, setStorageEpic, clearStorageEpic, resetLocalStorageEpic, syncWalletDataEpic];
+export default [getStorageEpic, setStorageEpic, clearStorageEpic, resetLocalStorageEpic];
