400ae3d1d0a117f9d44fa8a1f9c95bad68f757f0
Ken Huang <ken.huang@centrality.ai>
2019-02-18 11:41:25 +1300

    fix switch network issues

--- app/renderer/epics/homePage.epics.js
+++ app/renderer/epics/homePage.epics.js
@@ -20,10 +20,6 @@ const homePageLoadEpic = (action$, state$) =>
         return { type: types.navigation.triggered, payload: ROUTES.TERMS_OF_USE_ACCEPTANCE };
       }
 
-
-      console.log('homePageLoadEpic');
-      console.log(isNetworkRemembered);
-      console.log(selectedNetwork);
       if (isNetworkRemembered && selectedNetwork) {
         if (
           (selectedNetwork === NetworkNameOptions.LOCAL_TESTNET && genesisConfigFilePath) ||
