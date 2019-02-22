c0d7f0abcdf3d6c50f1e9baf7e13dca5de084a67
Ben Gao <xingbingao@gmail.com>
2019-02-22 22:13:09 +1300

    Refactor launcher config

--- app/main/launcherConfig.js
+++ app/main/launcherConfig.js
@@ -1,7 +1,6 @@
 // @flow
 import path from 'path';
 import readLauncherConfig from './utils/readLauncherConfig';
-import { environment } from './environment';
 
 const { LAUNCHER_CONFIG } = process.env;
 
@@ -30,10 +29,3 @@ export const APP_NAME = 'Odin';
 export const launcherConfig: LauncherConfig = readLauncherConfig(LAUNCHER_CONFIG);
 export const appLogsFolderPath = launcherConfig.logsPrefix;
 export const pubLogsFolderPath = path.join(appLogsFolderPath, 'pub');
-
-// CennetNetNode config
-export const NODE_STARTUP_TIMEOUT = 5000;
-export const NODE_STARTUP_MAX_RETRIES = 5;
-export const NODE_SHUTDOWN_TIMEOUT = environment.isTest ? 5000 : 10000;
-export const NODE_KILL_TIMEOUT = environment.isTest ? 5000 : 10000;
-export const NODE_UPDATE_TIMEOUT = environment.isTest ? 10000 : 60000;
