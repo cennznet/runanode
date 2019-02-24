31cca6ce910a26f2237a7007477a3a176391b4d4
Cherry Liang <cherry@centrality.ai>
2019-02-25 11:01:16 +1300

    modal refactor

---
+++ app/renderer/components/Modal/lib/modalFooter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from 'renderer/theme';
+
+const ModalFooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row-reverse;
+  padding: 1rem;
+  border-top: ${p => `1px solid ${colors.N500}`};
+`;
+
+const ModalFooter = ({ children }) => <ModalFooterWrapper>{children}</ModalFooterWrapper>;
+
+export default ModalFooter;
