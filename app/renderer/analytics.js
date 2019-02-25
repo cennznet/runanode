950e8f7e82b67cfa870795fe32b8b8cf5be559ee
Ben Gao <xingbingao@gmail.com>
2019-02-26 09:36:23 +1300

    Refactor analytics

--- app/renderer/analytics.js
+++ app/renderer/analytics.js
@@ -4,30 +4,42 @@ import appVersion from '../../package.json';
 
 const storage = global.electronStore;
 const userId = 'GA_USER_ID';
+const ANALYTICS_DISABLED = 'ANALYTICS_DISABLED';
 
 export const enableGoogleAnalytics = () => {
-  console.log('enabled google analytics');
   if (!storage.get(userId)) {
     storage.set(userId, uuid());
   }
 
-  if (window.ga) {
-    window.ga_debug = { trace: true }; // Userd in ga debug mode
-    window.ga('create', config.gaTrackId, 'auto');
-    window.ga('set', 'checkProtocolTask', null);
-    window.ga('set', 'checkStorageTask', null);
-    window.ga('set', 'userId', storage.get(userId));
-    window.ga('set', 'dimension1', appVersion.version || '0.0.0');
+  console.log('window.ga before', window.ga);
+  console.log('window.ga() before', window.ga());
+
+  if (window.ga && window.ga() !== 'ANALYTICS_DISABLED') {
+    window.ga = window.ga;
   }
+
+  if (!window.ga) {
+    window.ga = function() {
+      (ga.q = ga.q || []).push(arguments);
+    };
+    ga.l = +new Date();
+  }
+
+  console.log('window.ga after', window.ga);
+  console.log('window.ga() after', window.ga());
+
+  // window.ga_debug = { trace: true }; // Userd in ga debug mode
+  window.ga('create', config.gaTrackId, 'auto');
+  window.ga('set', 'checkProtocolTask', null);
+  window.ga('set', 'checkStorageTask', null);
+  window.ga('set', 'userId', storage.get(userId));
+  window.ga('set', 'dimension1', appVersion.version || '0.0.0');
 };
 
 export const disableGoogleAnalytics = () => {
-  console.log('disabled google analytics');
   if (storage.get(userId)) {
     storage.delete(userId);
   }
 
-  if (window.ga) {
-    window.ga = null;
-  }
+  window.ga = () => 'ANALYTICS_DISABLED';
 };
