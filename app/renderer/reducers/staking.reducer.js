eb9438fcc12081a217913425acfded98148b3e23
Cherry Liang <cherry@centrality.ai>
2019-03-01 12:16:59 +1300

    add stakingOverview reducer

--- app/renderer/reducers/staking.reducer.js
+++ app/renderer/reducers/staking.reducer.js
@@ -1,20 +1,18 @@
 import R from 'ramda';
 import types from '../types';
 
-const DEFAULT_STATE = {
-  validators: [],
-  intensions: [],
-};
+const DEFAULT_STATE = {};
 
 export default function stakingStore(state = DEFAULT_STATE, { type, payload }) {
   switch (type) {
-    case types.networkStateChange.triggered:
+    case types.updateEraProgress.triggered:
+    case types.updateEraLength.triggered:
+    case types.updateSessionLength.triggered:
+    case types.updateSessionProgress.triggered:
+    case types.updateValidators.triggered:
+    case types.updateIntentions.triggered:
       return R.merge(state, {
-        state: payload,
-      });
-    case types.cenznetStatusChange.triggered:
-      return R.merge(state, {
-        status: payload,
+        ...payload,
       });
     default:
       return state;
