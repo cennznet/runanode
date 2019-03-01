d5a3aeb041780370202f61093a9e4d3f9bb62d29
Cherry Liang <cherry@centrality.ai>
2019-03-01 13:32:47 +1300

    add stakingOverview reducer

--- app/renderer/pages/stakingOverviewPage/index.js
+++ app/renderer/pages/stakingOverviewPage/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react';
 import { MainContent, MainLayout } from 'components/layout';
 import { PageHeading } from 'components';
+import styled from 'styled-components';
 import withContainer from './container';
+import StakingProgressCard from './StakingProgressCard';
 
-const StakingOverviewPage = ({ subNav, onSubscribeStakingData }) => {
+const StakingOverviewPage = ({
+  subNav,
+  onSubscribeStakingData,
+  staking: { eraProgress, eraLength, sessionLength, sessionProgress },
+}) => {
   useEffect(() => {
     onSubscribeStakingData();
   }, []);
@@ -12,7 +18,12 @@ const StakingOverviewPage = ({ subNav, onSubscribeStakingData }) => {
     <MainLayout subNav={subNav}>
       <MainContent>
         <PageHeading>Staking overview</PageHeading>
-        <div>overview</div>
+        <StakingProgressCard
+          eraProgress={eraProgress}
+          eraLength={eraLength}
+          sessionLength={sessionLength}
+          sessionProgress={sessionProgress}
+        />
       </MainContent>
     </MainLayout>
   );
