9ba93695f7dc0d3f7df43908819960bef3f4bd24
Ben Gao <xingbingao@gmail.com>
2019-03-04 10:10:48 +1300

    Refactor ipc channel epic

--- app/renderer/routes/Route.js
+++ app/renderer/routes/Route.js
@@ -1,31 +1,14 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route as ReactRoute } from 'react-router-dom';
-import types from 'renderer/types';
 
 const mapStateToProps = ({ nodeStateStore }) => ({
   nodeState: nodeStateStore,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onIpcNodeState: payload => {
-    dispatch({
-      type: types.ipcNodeState.triggered,
-      payload,
-    });
-  },
-});
+const mapDispatchToProps = dispatch => ({});
 
 const Route = ({ children, nodeState, ...props }) => {
-  useEffect(() => {
-    props.onIpcNodeState('SUBSCRIBE');
-    return () => {
-      props.onIpcNodeState('UNSUBSCRIBE');
-    };
-  });
-
-  console.log('nodeState', nodeState);
-
   return <ReactRoute {...props}>{children}</ReactRoute>;
 };
 
