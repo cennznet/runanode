5b9e84057028d35ab4518567fa62dcc5ae626b5e
Cherry Liang <cherry@centrality.ai>
2019-03-07 13:55:45 +1300

    add multiple-select in stakingAccountPage

--- app/renderer/pages/stakingStakePage/SelectStakingAccount.js
+++ app/renderer/pages/stakingStakePage/SelectStakingAccount.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Select } from 'components';
 import styled from 'styled-components';
 import { colors } from 'renderer/theme';
+import { getConstantValue } from 'typescript';
+
+const StepDescription = styled.div`
+  margin: 1.5rem 0;
+`;
+
+const SelectTitle = styled.div`
+  margin: 0.5rem 0;
+  font-weight: 600;
+  font-size: 16px;
+`;
 
 const WalletName = styled.div`
   font-size: 14px;
@@ -26,10 +37,16 @@ const SelectStakingAccount = ({ wallets, onSelectFn }) => {
 
   return (
     <div>
+      <StepDescription>
+        Step 1: Start staking by choosing an account from dropdown.{' '}
+      </StepDescription>
+      <SelectTitle>Choose an account to stake</SelectTitle>
       <Select
-        placeholder="Please select an account to stake"
+        placeholder="Select from dropdown."
         options={groupedOptions}
         formatGroupLabel={data => <WalletName>{data.label}</WalletName>}
+        styles={{ option: styles => ({ ...styles, paddingLeft: '1.5rem' }) }}
+        onChange={value => console.log(getConstantValue)}
       />
     </div>
   );
