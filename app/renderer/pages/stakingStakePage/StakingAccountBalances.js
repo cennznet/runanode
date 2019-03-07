c523930536a12040928a1c88debab2ab178b13ea
Cherry Liang <cherry@centrality.ai>
2019-03-07 15:18:29 +1300

    select account to start stake

---
+++ app/renderer/pages/stakingStakePage/StakingAccountBalances.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Select } from 'components';
+import styled from 'styled-components';
+import { colors } from 'renderer/theme';
+
+const StakingAccountBalances = ({ stakingAccount }) => {
+  return (
+    <div>
+      <div>CENNZ Balances</div>
+      <div>CENTRAPAY Balances</div>
+    </div>
+  );
+};
+
+export default StakingAccountBalances;
