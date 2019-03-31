fa186ea268306dadbdd782a254a5af16fe08b59c
Ben Gao <xingbingao@gmail.com>
2019-04-01 08:40:08 +1300

    Replace prod GA tracking id

--- app/config/production.js
+++ app/config/production.js
@@ -8,7 +8,7 @@ const feature = {
 const prodConfig = {
   feature,
   // prod specific config comes here...
-  gaTrackId: 'UA-132943388-1', // TODO: Change to UA-132910586-1 at the time of publishing
+  gaTrackId: 'UA-132910586-1',
 };
 
 export default mergeOptions(commonConfig, prodConfig);
