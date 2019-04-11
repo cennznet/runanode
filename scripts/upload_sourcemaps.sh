a325efe6667126f35c0fd8c9d6287f643eec96de
Ken Huang <ken.huang@centrality.ai>
2019-04-11 16:41:04 +1200

    Add sentry integration

--- scripts/upload_sourcemaps.sh
+++ scripts/upload_sourcemaps.sh
@@ -3,16 +3,12 @@
 # https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/1560#issuecomment-439576398
 echo "Uploading source maps to Sentry"
 
-#source env.internal
-echo "SENTRY_ORG: $SENTRY_ORG"
 export SENTRY_ORG=centrality-ai
-echo "SENTRY_AUTH_TOKEN: $SENTRY_AUTH_TOKEN"
+echo "SENTRY_ORG: $SENTRY_ORG"
 export SENTRY_AUTH_TOKEN=fa4017b1975c4defb9951c7f978ef02e6251e43679a34ea193d867c24b46dc15
-
-#source env.production
-echo "SENTRY_PROJECT: $SENTRY_PROJECT"
+echo "SENTRY_AUTH_TOKEN: $SENTRY_AUTH_TOKEN"
 export SENTRY_PROJECT=cenznet-node-ui
-
+echo "SENTRY_PROJECT: $SENTRY_PROJECT"
 export GIT_HASH=`git rev-parse HEAD`
 echo "GIT_HASH: $GIT_HASH"
 
