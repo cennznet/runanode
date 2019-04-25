948459ab2835227ff307c1469eb4ea44771c2622
Ben Gao <xingbingao@gmail.com>
2019-04-26 10:34:47 +1200

    Refactor Input component theming

---
+++ app/renderer/components/defaultTheme/components.js
@@ -0,0 +1,51 @@
+const configComponentStyle = colors => ({
+  // Button: {
+  //   fontSize: {
+  //     sm: '14px',
+  //     md: '14px',
+  //     lg: '1rem',
+  //   },
+  //   height: {
+  //     sm: '1.5rem',
+  //     md: '2rem',
+  //     lg: '2.5rem',
+  //   },
+  //   color: {
+  //     primary: colors.primary,
+  //     danger: colors.danger,
+  //     success: colors.success,
+  //     warning: colors.warning,
+  //     info: colors.info,
+  //   },
+  //   hoverColor: {
+  //     primary: colors.B600,
+  //     danger: colors.R600,
+  //     success: colors.G600,
+  //     warning: colors.Y600,
+  //     info: colors.B600,
+  //   },
+  //   contrastColor: {
+  //     primary: colors.N0,
+  //     danger: colors.N0,
+  //     success: colors.N0,
+  //     warning: colors.N800,
+  //     info: colors.N0,
+  //   },
+  // },
+  // Input: {
+  //   borderColor: colors.border,
+  //   background: '#FFFFFF',
+  //   readOnlyBackground: '#CCC',
+  //   readOnlyBorderColor: '#DDD',
+  //   placeholderColor: '#AAA',
+  //   color: colors.N800,
+  //   focusBorderColor: colors.B500,
+  //   size: {
+  //     sm: '2rem',
+  //     md: '3rem',
+  //     lg: '4rem',
+  //   },
+  // },
+});
+
+export default configComponentStyle;
