6eae177cdc4762958f6f370a7e690a4e6143e17d
Cherry Liang <cherry@centrality.ai>
2019-04-29 10:45:55 +1200

    make Modal/Toggle component themeable

--- app/renderer/components/Toggle/index.js
+++ app/renderer/components/Toggle/index.js
@@ -5,52 +5,57 @@ import styledProps from 'styled-props';
 import mergeOptions from 'merge-options';
 import ReactToggle from 'react-toggle';
 import 'react-toggle/style.css';
-import themeObj from 'renderer/theme';
+import theme from 'components/defaultTheme';
 
-const defaultStyling = p => {
+const CustomToggle = ({ themeStyles, themeSpace, color, ...restProps }) => (
+  <ReactToggle icons={false} {...restProps} />
+);
+
+const defaultThemeStyle = p => {
   const { colors } = p.theme;
 
   return {
-    color: {
+    variant: {
       primary: colors.primary,
       danger: colors.danger,
       success: colors.success,
       warning: colors.warning,
     },
+
+    trackBackground: colors.N300,
+    thumbBorderCorlor: colors.N300,
+    thumbCheckedBorderColor: colors.N0,
+    thumbDisabledBackground: colors.N300,
   };
 };
 
-const styling = p => mergeOptions({}, defaultStyling(p), p.theme[p.themeSpace], p.themeStyles);
-
-const CustomToggle = ({ theme, themeStyles, themeSpace, color, ...restProps }) => (
-  <ReactToggle icons={false} {...restProps} />
-);
+const computedThemeStyle = p => p.theme.utils.createThemeStyle(p, defaultThemeStyle);
 
 const Toggle = styled(CustomToggle)`
   &.react-toggle {
     .react-toggle-track {
       width: 40px;
       height: 20px;
-      background: ${p => p.theme.colors.N300};
+      background: ${p => computedThemeStyle(p).trackBackground};
     }
 
     .react-toggle-thumb {
       width: 18px;
       height: 18px;
-      border: ${p => `1px solid ${p.theme.colors.N300}`};
+      border: ${p => `1px solid ${computedThemeStyle(p).thumbBorderCorlor}`};
     }
 
     &.react-toggle--checked {
       .react-toggle-track {
-        background: ${p => styledProps(styling(p).color, 'color')};
+        background: ${p => styledProps(computedThemeStyle(p).variant, 'variant')};
       }
 
       .react-toggle-thumb {
-        width: 16px;
-        height: 16px;
-        top: 2px;
-        left: 22px;
-        border-color: #fff;
+        width: 16px,
+        height: 16px,
+        top: 2px,
+        left: 22px,
+        border-color: ${p => computedThemeStyle(p).thumbCheckedBorderColor};
       }
     }
 
@@ -70,12 +75,12 @@ const Toggle = styled(CustomToggle)`
 
     &:hover&:not(.react-toggle--disabled) {
       .react-toggle-track {
-        background: ${p => styledProps(styling(p).color, 'color')};
+        background: ${p => styledProps(computedThemeStyle(p).variant, 'variant')};
       }
 
       :not(.react-toggle--checked) {
         .react-toggle-track {
-          background: ${p => p.theme.colors.N300};
+          background: ${p => computedThemeStyle(p).thumbDisabledBackground};
         }
       }
     }
@@ -83,14 +88,21 @@ const Toggle = styled(CustomToggle)`
 `;
 
 Toggle.defaultProps = {
-  color: 'success',
-  theme: themeObj,
-  themeSpace: 'toggle',
-  themeStyles: {},
+  variant: 'success',
+  theme,
+  themeKey: 'Toggle',
 };
 
 Toggle.propTypes = {
   color: PropTypes.oneOf(['primary', 'success', 'warning', 'danger']),
+  theme: PropTypes.object,
+  themeKey: PropTypes.string,
+  themeStyle: PropTypes.object,
+  trackBackground: PropTypes.string,
+  thumbBorderCorlor: PropTypes.string,
+  thumbCheckedBorderColor: PropTypes.string,
+  thumbDisabledBackground: PropTypes.string,
 };
 
+/** @component */
 export default Toggle;
