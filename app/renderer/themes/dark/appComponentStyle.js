0fba548c1b7459228b7624e000ccc7e9d80cf44c
Cherry Liang <cherry@centrality.ai>
2019-05-01 08:43:17 +1200

    add light theme to the app

--- app/renderer/themes/dark/appComponentStyle.js
+++ app/renderer/themes/dark/appComponentStyle.js
@@ -5,7 +5,7 @@ const configComponentStyle = colors => ({
   //     md: '14px',
   //     lg: '1rem',
   //   },
-  //   height: {
+  //   size: {
   //     sm: '1.5rem',
   //     md: '2rem',
   //     lg: '2.5rem',
@@ -32,17 +32,101 @@ const configComponentStyle = colors => ({
   //     info: colors.N0,
   //   },
   // },
-  Input: {
-    // borderColor: colors.V400,
-    // background: 'rgba(114,94,255,0.5)',
-    // readOnlyBackground: 'red',
-    // readOnlyBorderColor: 'yellow',
-    // color: colors.N0,
-    // focusBorderColor: colors.V500,
-    // size: {
-    //   md: '3rem',
-    // },
-  },
+  // Checkbox: {
+  //   color: {
+  //     primary: colors.primary,
+  //     danger: colors.danger,
+  //     success: colors.success,
+  //     warning: colors.warning,
+  //   },
+  // },
+  // Input: {
+  //   borderColor: colors.border,
+  //   background: '#FFFFFF',
+  //   readOnlyBackground: '#CCC',
+  //   readOnlyBorderColor: '#DDD',
+  //   placeholderColor: '#AAA',
+  //   color: colors.N800,
+  //   focusBorderColor: colors.B500,
+  //   size: {
+  //     sm: '2rem',
+  //     md: '3rem',
+  //     lg: '4rem',
+  //   },
+  // },
+  // Modal: {
+  //   backgroundColor: colors.V900,
+  //   boxShadow: `0 2px 4px 0 ${colors.N900}`,
+  //   overlayBackground: 'rgba(54, 58, 61, 0.7)',
+  //   color: colors.N0,
+  //   contentPadding: '2rem',
+  //   footerPadding: '1rem',
+  //   footerBorderTopColor: `1px solid ${colors.border}`,
+  // },
+  // Notification: {
+  //   background: {
+  //     info: colors.info,
+  //     danger: colors.danger,
+  //     success: colors.success,
+  //     warning: colors.warning,
+  //   },
+  //   color: {
+  //     info: colors.N800,
+  //     danger: colors.N0,
+  //     success: colors.N0,
+  //     warning: colors.N800,
+  //   },
+  // Scrollable: {
+  //   background: colors.V800,
+  //   thumbBackground: 'rgba(255, 255, 255, 0.4)',
+  //   gradientBottomBackground: 'linear-gradient(180deg, rgba(4, 12, 64, 0) 0%, #040c40 100%)',
+  // },
+  // Spinner: {
+  //   color: colors.primary,
+  //   size: {
+  //     xs: '10px',
+  //     sm: '12px',
+  //     md: '16px',
+  //     lg: '24px',
+  //     xl: '40px',
+  //   },
+  //   thickness: {
+  //     xs: '2px',
+  //     sm: '2px',
+  //     md: '2px',
+  //     lg: '2px',
+  //     xl: '4px',
+  //   },
+  //   speed: '0.7s',
+  // },
+  // Table: {
+  //   headerBackground: colors.V900,
+  //   rowOddLineBackground: 'rgba(8, 24, 127, 0.7)',
+  //   rowEvenLineBackground: 'rgba(8, 24, 127, 0.3)',
+  // },
+  // Tabs: {
+  //   activeColor: colors.text,
+  //   color: colors.textMuted,
+  //   inkBarActiveColor: colors.primary,
+  //   inkBarColor: colors.border,
+  //   tabPaneHeight: '80vh',
+  // },
+  // Toggle: {
+  //   variant: {
+  //     primary: colors.primary,
+  //     danger: colors.danger,
+  //     success: colors.success,
+  //     warning: colors.warning,
+  //   },
+  //   trackBackground: colors.N300,
+  //   thumbBorderCorlor: colors.N300,
+  //   thumbCheckedBorderColor: colors.N0,
+  //   thumbDisabledBackground: colors.N300,
+  // },
+  // Tooltip: {
+  //   background: '#A99DFF',
+  //   color: colors.V900,
+  // },
 });
 
 export default configComponentStyle;
