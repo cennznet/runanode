ec4a8f0e07ffc9e5c6e4ba811a710973372c7e38
Ben Gao <xingbingao@gmail.com>
2019-05-24 13:06:01 +1200

    Delete usable files

--- testHelper/testSetup.js
+++
@@ -1,9 +0,0 @@
-import Adapter from 'enzyme-adapter-react-16';
-import { configure, shallow, render, mount } from 'enzyme';
-
-configure({ adapter: new Adapter() });
-
-// Make Enzyme functions available in all test files without importing
-global.shallow = shallow;
-global.render = render;
-global.mount = mount;
